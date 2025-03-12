3c5aae62020a69f639466cea39118ad890f690c3
peekagrub <25369813+peekagrub@users.noreply.github.com>
2025-03-12 13:35:26 +0100

    Add count and speed sliders

--- src/Screensaver.cs
+++ src/Screensaver.cs
@@ -15,7 +15,9 @@ public class GlobalSettings
 {
     public bool enabled = true;
     public bool changeColorOnBounce = true;
+    public int count = 1;
     public float screenPercentage = 0.25f;
+    public float speed = 0.15f;
 }
 
 public class Screensaver : Mod, IGlobalSettings<GlobalSettings>, ICustomMenuMod
@@ -24,7 +26,7 @@ public class Screensaver : Mod, IGlobalSettings<GlobalSettings>, ICustomMenuMod
 
     public void OnLoadGlobal(GlobalSettings s) {
         settings = s;
-        if (behaviour != null)
+        foreach (var behaviour in behaviours)
         {
             behaviour.ToggleScreensaver(settings.enabled);
             behaviour.ToggleColorOnBounce(settings.changeColorOnBounce);
@@ -35,7 +37,8 @@ public class Screensaver : Mod, IGlobalSettings<GlobalSettings>, ICustomMenuMod
     new public string GetName () => "Screensaver";
     public override string GetVersion () => "1.1.0.1";
 
-    private ScreensaverBehaviour behaviour;
+    private GameObject SSObj;
+    private List<ScreensaverBehaviour> behaviours = new();
     private static Screensaver _instance;
 
     public static Screensaver Instance {
@@ -45,11 +48,54 @@ public class Screensaver : Mod, IGlobalSettings<GlobalSettings>, ICustomMenuMod
     public override void Initialize ()
     {
         _instance = this;
-        GameObject SSObj = new GameObject();
-        behaviour = SSObj.AddComponent<ScreensaverBehaviour>();
+        SSObj = new GameObject();
+        for (int i = 0; i < settings.count; i++)
+        {
+            var behaviour = SSObj.AddComponent<ScreensaverBehaviour>();
+            behaviour.ToggleScreensaver(settings.enabled);
+            behaviour.ToggleColorOnBounce(settings.changeColorOnBounce);
+            behaviours.Add(behaviour);
+        }
         Object.DontDestroyOnLoad(SSObj);
-        behaviour.ToggleScreensaver(settings.enabled);
-        behaviour.ToggleColorOnBounce(settings.changeColorOnBounce);
+    }
+
+    private void UpdateToggle()
+    {
+        foreach (var behaviour in behaviours)
+        {
+            behaviour.ToggleScreensaver(settings.enabled);
+        }
+    }
+
+    private void UpdateColorOnBounce()
+    {
+        foreach (var behaviour in behaviours)
+        {
+            behaviour.ToggleColorOnBounce(settings.changeColorOnBounce);
+        }
+    }
+
+    private void UpdateCount()
+    {
+        if (behaviours.Count > settings.count)
+        {
+            for (int i = behaviours.Count - 1; i >= settings.count; i--)
+            {
+                var behaviour = behaviours[i];
+                behaviours.RemoveAt(i);
+                Object.Destroy(behaviour);
+            }
+        }
+        else if (behaviours.Count < settings.count)
+        {
+            for (int i = behaviours.Count; i < settings.count; i++)
+            {
+                var behaviour = SSObj.AddComponent<ScreensaverBehaviour>();
+                behaviour.ToggleScreensaver(settings.enabled);
+                behaviour.ToggleColorOnBounce(settings.changeColorOnBounce);
+                behaviours.Add(behaviour);
+            }
+        }
     }
 
     private Menu menuRef;
@@ -57,22 +103,30 @@ public class Screensaver : Mod, IGlobalSettings<GlobalSettings>, ICustomMenuMod
     private Menu PrepareMenu()
     {
         return new Menu("Screensaver", new Element[]{
-            new  HorizontalOption("Screensaver", 
+            Blueprints.HorizontalBoolOption("Screensaver",
                 "Toggle if the screensaver is active",
-                new string[] {"On", "Off"},
-                (option) => { settings.enabled = option == 0; behaviour.ToggleScreensaver(settings.enabled); },
-                () => settings.enabled ? 0 : 1
+                (option) => { settings.enabled = option; UpdateToggle(); },
+                () => settings.enabled
             ),
-            new  HorizontalOption("Change color on bounce", 
+            Blueprints.HorizontalBoolOption("Change color on bounce", 
                 "Toggle if the color of the screensaver should change on bounce",
-                new string[] {"On", "Off"},
-                (option) => { settings.changeColorOnBounce = option == 0; behaviour.ToggleColorOnBounce(settings.changeColorOnBounce); },
-                () => settings.changeColorOnBounce ? 0 : 1
+                (option) => { settings.changeColorOnBounce = option; UpdateColorOnBounce(); },
+                () => settings.changeColorOnBounce
+            ),
+            new FixedCustomSlider("Screensaver Count",
+                (option) => { settings.count = (int)option; UpdateCount(); },
+                () => settings.count,
+                1f, 9.0f, true
             ),
             new PercentSlider("Screen Percentage",
                 (option) => settings.screenPercentage = option,
                 () => settings.screenPercentage,
                 0.01f, 0.5f, false
+            ),
+            new FixedCustomSlider("Screensaver Speed",
+                (option) => settings.speed = option,
+                () => settings.speed,
+                0.1f, 2.0f, false
             ),
         });
     }
